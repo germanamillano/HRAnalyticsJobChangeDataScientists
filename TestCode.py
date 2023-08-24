9cd0d436884c337040af431fa8d4a1a624b4e711
germanamillano <140355075+germanamillano@users.noreply.github.com>
2023-08-24 09:18:37 -0700

    Files updated for logs with Singleton

--- TestCode.py
+++ TestCode.py
@@ -1,4 +1,6 @@
-import pandas as pd
+from hranalyticsjobchangedatascientists.hranalyticsjobchangedatascientists.logs.MyLogger import \
+    MyLogger
 
-train_data = pd.read_csv('Datasets/aug_train.csv')
-test_data = pd.read_csv('Datasets/aug_test.csv')
+logger = MyLogger.__call__().get_logger()
+logger.info("Hello, Logger")
+logger.debug("bug occured")
